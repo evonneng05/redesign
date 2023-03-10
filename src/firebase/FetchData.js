779684fe15ff999eaa640c655d3451e4f506dd70
Wong Yu Fei <ywong045@e.ntu.edu.sg>
2023-03-10 18:19:57 +0800

    update records functionality

--- src/firebase/FetchData.js
+++ src/firebase/FetchData.js
@@ -1,12 +1,44 @@
 import { useCollection } from '../firebase/useCollection'
 import { useAuthContext } from "./useAuthContext" 
+import { UserRecords } from './UserRecordsObject'
+import { doc, setDoc, updateDoc } from 'firebase/firestore' 
+import { db } from './config'
+
 export default function FetchData() {
     const {user} = useAuthContext()
     const {documents: users} = useCollection('users',['uid','==',user.uid])
+
+    function processData(){ //load user data
+        var record= new UserRecords(users[0])
+        return record
+    }
+    async function updateData(){ //update user data
+        var record= processData()
+        //new update is score for round 5 being 5
+        record.setScore(4,5)
+        const ref=doc(db,'users','darren')
+        await updateDoc(ref,{
+            score:record.score,
+          })
+    }
+    async function addStuff(){ //create new user function
+        const ref=doc(db,'users','darren')
+        await setDoc(ref,{
+            name:'darren',
+            score:[5,5,5,5,10],
+            uid: user.uid
+          })
+      }
   return (
     <div>
-      <button onClick={()=>console.log(users)}>
+        <button onClick={addStuff}>
+            Add Data
+        </button>
+        <button onClick={processData}>
             Get Data
+        </button>
+        <button onClick={updateData}>
+            Update Data
         </button>
     </div>
   )
--- src/firebase/functions.js
+++ src/firebase/functions.js
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 import { useFetch } from '../hooks/useFetch'; //use this function to fetch async data- usage: const {data: data, isPending, error}= useFetch('http://localhost:3000/user')
+
 //user authentication functions
 import { useSignup } from './useSignup'//sign up with email and password
 import { useLogout } from "./useLogout"; //log out
 import { useLogin } from "./useLogin"; //sign in with email and password
 import { useAuthContext } from "./useAuthContext" //used to check the user's current authentication status
-//fetch user data functions
-import { db } from './config';
-import { collection, addDoc } from 'firebase/firestore' 
 import FetchData from "./FetchData";
 
 export default function Functions() {
@@ -21,36 +19,24 @@ export default function Functions() {
   const {error_login, login}= useLogin()
   const {logout}= useLogout()
 
-
   const handleSubmit = (e) => {
     e.preventDefault()
     signup(email,password)
   }
 
-  async function addStuff(){
-    const ref=collection(db,'users')
-    await addDoc(ref,{
-        name:'darren',
-        score:[5,5,5,5,10],
-        uid: user.uid
-      })
-  }
-  
   //fetchData is conditional on the user's uid so need to check that he is registered
   return (
     <div>
-        {user&& <FetchData/>}  
-        <button onClick={addStuff}>
-            Add Data
-        </button>
+        {user && <FetchData/>}  
         <button onClick={()=>login('test@gmail.com','test12345')}>
-            sign in
+            Sign in
         </button>
+        {error_login && <p>{error_login}</p>}
         <button onClick={()=>console.log(user)}>
-            check status
+            Check status
         </button>
         <button onClick={()=>logout()}>
-            log out
+            Log Out
         </button>
         <h2>Signup</h2>
         <form onSubmit={handleSubmit}>
