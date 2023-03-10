f5f69b134f858d2df2d724f5026a2ea68af2d27f
Wong Yu Fei <ywong045@e.ntu.edu.sg>
2023-03-10 23:34:55 +0800

    game progression logic

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -3,8 +3,8 @@ import StarFilled from "../assets/StarFilled.svg";
 import StarEmpty from "../assets/StarEmpty.svg";
 
 function Stars(props) {
-  const StarElement=props.count.map((value)=>
-    <img className="star" src={value==1?StarFilled:StarEmpty} alt="Star1" />
+  const StarElement=props.count.map((value, idx)=>
+    <img key= {idx} className="star" src={value==1?StarFilled:StarEmpty} alt="Star1" />
   );
   return (
     <div className="star-container">
