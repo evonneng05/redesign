4ed0654d507cb5471b1f5034d46c6e1c814ba502
Wong Yu Fei <ywong045@e.ntu.edu.sg>
2023-03-10 21:50:18 +0800

    rendering of stars, score and text for scorepage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,16 @@ import Titlepage from "./Titlepage";
 import LevelTag from "./LevelTag";
 import MapPage from "./MapPage";
 import ScorePage from "./ScorePage";
+import { UserRecords } from "../firebase/UserRecordsObject";
+
 function App() {
-  return <ScorePage />;
+  var score=1;
+  var level=1;
+
+  return <ScorePage 
+    score= {score}
+    level={level}
+  />;
 }
 
 export default App;
